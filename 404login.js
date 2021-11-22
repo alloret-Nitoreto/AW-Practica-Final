3171c6fefdd144cb5476d52364f209d6e981f145
Alvaro <lei.lioret@gmail.com>
2021-11-22 17:33:36 +0100

    Intento guardar base de datos

--- 404login.js
+++ 404login.js
@@ -3,8 +3,18 @@ const http = require("http");
 const url = require("url");
 const path = require("path");
 const express = require("express");
+const multer = require("multer");
 
 const app = express();
+const multerFactory = multer({ storage: multer.memoryStorage() });
+
+const mysql = require("mysql");
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "practicafinal"
+});
 
 app.set("view engine", "ejs");
 
@@ -14,17 +24,17 @@ const ficherosEstaticos =
 app.use(express.static(ficherosEstaticos));
 
 
-app.get("/", function(request, response) {
+app.get("/", function (request, response) {
     response.redirect("/404login.html");
 });
 
 
-app.get("/crear_cuenta", function(request, response) {
+app.get("/crear_cuenta", function (request, response) {
     response.redirect("/registro.html");
 });
 
 
-app.listen(3000, function(err) {
+app.listen(3000, function (err) {
     if (err) {
         console.error("No se pudo inicializar el servidor: " +
             err.message);
@@ -32,3 +42,42 @@ app.listen(3000, function(err) {
         console.log("Servidor arrancado en el puerto 3000");
     }
 });
+
+app.post("/procesar_formulario", multerFactory.single('foto'),
+    function (request, response) {
+        let usuario = {
+            nombre: request.body.nombre,
+            apellidos: request.body.apellidos,
+            fumador: request.body.fumador === "si" ? "Sí" : "No",
+            imagen: null
+        };
+        if (request.file) {
+            usuario.imagen = request.file.buffer;
+        }
+        insertarUsuario(usuario, function (err, newId) {
+            if (!err) {
+                usuario.id = newId;
+                response.render("datosFormularioBD", usuario);
+            }
+        });
+    });
+
+function insertarUsuario(usuario, callback) {
+    pool.getConnection(function (err, con) {
+        if (err)
+            callback(err);
+        else {
+            let sql =
+                "INSERT INTO personas(Nombre, Apellidos, Fumador, Foto) VALUES(?, ?, ?, ?)";
+            con.query(sql, [usuario.nombre, usuario.apellidos,
+            usuario.fumador, usuario.imagen],
+                function (err, result) {
+                    con.release();
+                    if (err)
+                        callback(err);
+                    else
+                        callback(null, result.insertId);
+                });
+        }
+    });
+}
