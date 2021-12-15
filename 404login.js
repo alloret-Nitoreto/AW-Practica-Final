a2f7173844b85bf66118e0cbff81aed98992c53a
Nitoreto <lei.lioret@gmail.com>
2021-12-15 14:06:09 +0100

    Ya funciona las etiquetas y buscar

--- 404login.js
+++ 404login.js
@@ -13,13 +13,13 @@ const app = express();
 const middlewareSession = session({
     saveUninitialized: false,
     secret: "foobar34",
-    resave:false
+    resave: false
 });
 
 app.use(middlewareSession);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const multerFactory = multer({ storage: multer.memoryStorage() });
 
@@ -44,28 +44,23 @@ app.use(express.static(ficherosEstaticos));
 //-----------------GET-------------------------
 
 app.get("/", function (request, response) {
-    response.render("404login.ejs", {errores:{}});
+    response.render("404login.ejs", { errores: {} });
 });
 
 app.get("/crear_cuenta", function (request, response) {
-    response.render("registro.ejs", {errores:{}});
+    response.render("registro.ejs", { errores: {} });
 });
 
 app.get("/ir_inicar_sesion", function (request, response) {
-    response.render("404login.ejs", {errores:{}});
-});
-
-app.get("/etiqueta/:etiqueta", function (request, response) {
-    let etiqueta = String(request.params.etiqueta);
-    obtenerPreguntas('etiqueta',etiqueta,request, response);
+    response.render("404login.ejs", { errores: {} });
 });
 
 app.get("/preguntas", function (request, response) {
-    obtenerPreguntas('todo', '',request, response);
+    obtenerPreguntas('todo', '', request, response);
 });
 
 app.get("/formularPregunta", function (request, response) {
-    response.render("formularPregunta.ejs", {usuario:request.session.usuario, errores:{}});
+    response.render("formularPregunta.ejs", { usuario: request.session.usuario, errores: {} });
 });
 
 //-----------------GET---------------------------
@@ -77,54 +72,54 @@ app.post('/inicar_sesion',
     // El campo login ha de ser no vacío.
     check("email", "Este campo no puede estar vacío").notEmpty(),
     // El campo email debe tener formato de correo
-    check("email","No es un correo electronico valido").isEmail(),
+    check("email", "No es un correo electronico valido").isEmail(),
     // El campo password ha de ser no vacío.
     check("password", "Este campo no puede estar vacío").notEmpty(),
     (request, response) => {
 
-    let usuario = {
-        email: request.body.email,
-        password: request.body.password,
-        foto: null,
-        nickname: null,
-        id: null
-    };
-    const errors = validationResult(request);
+        let usuario = {
+            email: request.body.email,
+            password: request.body.password,
+            foto: null,
+            nickname: null,
+            id: null
+        };
+        const errors = validationResult(request);
 
-    if(errors.isEmpty()){
-        pool.getConnection(function(err, connection) { 
-            if (err) {  
-                callback(new Error("Error de acceso a la base de datos:" + err));  
-            } 
-            else { 
-            connection.query("SELECT * FROM usuarios WHERE email = ? AND password = ?" , 
-            [usuario.email, usuario.password], 
-            function(err, result) { 
-                connection.release(); // devolver al pool la conexión 
-                if (err) { 
-                    callback(new Error("Error al inicar sesion:" + err)); 
-                } 
-                else { 
-                    if (result.length === 0) { 
-                        console.log("No es correcta la contraseña o el mail"); //no está el usuario con el password proporcionado 
-                    } 
-                    else { 
-                        usuario.foto = Buffer.from(result[0].foto).toString('base64'); 
-                        usuario.nickname = result[0].nickName;
-                        usuario.id = result[0].id;
-                        request.session.usuario = usuario;
-                        response.render("mainpage.ejs", {usuario});
-                    }            
-                } 
-            }); 
-            } 
-        } 
-        ); 
-    } else {
-        let er = errors.mapped();
-        response.render("404login.ejs", {errores: errors.mapped()});    
-    }
-});
+        if (errors.isEmpty()) {
+            pool.getConnection(function (err, connection) {
+                if (err) {
+                    callback(new Error("Error de acceso a la base de datos:" + err));
+                }
+                else {
+                    connection.query("SELECT * FROM usuarios WHERE email = ? AND password = ?",
+                        [usuario.email, usuario.password],
+                        function (err, result) {
+                            connection.release(); // devolver al pool la conexión 
+                            if (err) {
+                                callback(new Error("Error al inicar sesion:" + err));
+                            }
+                            else {
+                                if (result.length === 0) {
+                                    console.log("No es correcta la contraseña o el mail"); //no está el usuario con el password proporcionado 
+                                }
+                                else {
+                                    usuario.foto = Buffer.from(result[0].foto).toString('base64');
+                                    usuario.nickname = result[0].nickName;
+                                    usuario.id = result[0].id;
+                                    request.session.usuario = usuario;
+                                    response.render("mainpage.ejs", { usuario });
+                                }
+                            }
+                        });
+                }
+            }
+            );
+        } else {
+            let er = errors.mapped();
+            response.render("404login.ejs", { errores: errors.mapped() });
+        }
+    });
 //-------------------------Iniciar sesion------------------
 
 //-------------------- Crear cuenta -------------------------
@@ -133,7 +128,7 @@ app.post(
     // El campo login ha de ser no vacío.
     check("email", "Este campo no puede estar vacío").notEmpty(),
     // El campo email debe tener formato de correo
-    check("email","No es un correo electronico valido").isEmail(),
+    check("email", "No es un correo electronico valido").isEmail(),
     // El campo password ha de ser no vacío.
     check("password", "Este campo no puede estar vacío").notEmpty(),
     // El campo confimar password ha de ser no vacío.
@@ -157,22 +152,22 @@ app.post(
             }
             insertarUsuario(usuario, function (err) {
                 if (err) {
-                  console.log("Error de conexión a la base de datos" + err);
+                    console.log("Error de conexión a la base de datos" + err);
                 }
             });
         } else {
             let er = errors.mapped();
-            response.render("registro.ejs", {errores: errors.mapped()});    
+            response.render("registro.ejs", { errores: errors.mapped() });
         }
-});
+    });
 
 function insertarUsuario(usuario, callback) {
     pool.getConnection(function (err, con) {
         if (err)
             callback(err);
         else {
-            if(usuario.imagen === null){
-                usuario.imagen = fs.readFileSync(__dirname + '/imagenes/defecto1.png');      
+            if (usuario.imagen === null) {
+                usuario.imagen = fs.readFileSync(__dirname + '/imagenes/defecto1.png');
             }
             let sql =
                 "INSERT INTO usuarios(email, password, foto, nickName) VALUES(?, ?, ?, ?)";
@@ -182,9 +177,9 @@ function insertarUsuario(usuario, callback) {
                     con.release();
                     if (err)
                         callback(error);
-                    else{
+                    else {
                         console.log("CREADA CORRECTAMENTE")
-                        response.render("404login.ejs", {errores:{}});
+                        response.render("404login.ejs", { errores: {} });
                     }
                 });
         }
@@ -193,30 +188,31 @@ function insertarUsuario(usuario, callback) {
 //-------------------- Crear cuenta -------------------------
 
 //----------------------- Preguntas -----------------------------
+
 const sinEspacio = (param) => {
-    if(param != ""){
+    if (param != "") {
         let re = /\s/;
-        if(param.search(re)){
+        if (param.search(re)) {
             return true;
-        }else{
+        } else {
             return false;
         }
-    }else{
+    } else {
         return false;
     }
 };
 
 const max5 = (param) => {
-    if(param != ""){
+    if (param != "") {
         let re = /(?<=@)\w+/;
         let etiquetas = param.match(re);
-        if(etiquetas.length <= 5){
+        if (etiquetas.length <= 5) {
             param = etiquetas;
             return true;
-        }else{
+        } else {
             return false;
         }
-    }else{
+    } else {
         return false;
     }
 };
@@ -238,7 +234,7 @@ app.post(
             let pregunta = {
                 titulo: request.body.titulo,
                 cuerpo: request.body.cuerpo,
-                etiquetas: request.body.etiquetas,    
+                etiquetas: request.body.etiquetas,
                 fecha: d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate(),
                 usuarioId: request.session.usuario.id
             };
@@ -246,46 +242,54 @@ app.post(
             insertarPregunta(pregunta, function (err) {
                 if (err) {
                     console.log("Error de conexión a la base de datos" + err);
-                }else{
-                    response.render("preguntas.ejs", {usuario:request.session.usuario}); 
+                } else {
+                    response.render("preguntas.ejs", { usuario: request.session.usuario });
                 }
             });
         } else {
             let er = errors.mapped();
-            response.render("formularPregunta.ejs", {usuario:request.session.usuario, errores: errors.mapped()});    
+            response.render("formularPregunta.ejs", { usuario: request.session.usuario, errores: errors.mapped() });
         }
-});
+    });
+
+    app.post("/etiqueta", function (request, response) {
+        obtenerPreguntas('etiqueta', request.body.etiqueta , request, response);
+    });
+
+    app.post("/busqueda", function (request, response) {
+        obtenerPreguntas('busqueda', request.body.cuerpo , request, response);
+    });
 
 function obtenerPreguntas(tipoBusqueda, dato, request, response) {
     pool.getConnection(function (err, con) {
         if (err)
             callback(err);
         else {
-            let sqlAdd= '';
-            switch(tipoBusqueda){
+            let sqlAdd = '';
+            switch (tipoBusqueda) {
                 case 'todo': break;
-                case'etiqueta': sqlAdd = 'where '
-                case 'busqueda': sqlAdd = 'where'
+                case 'etiqueta': sqlAdd = ' WHERE p.etiquetas LIKE ?';dato = '%@' + dato + '%' ; break;
+                case 'busqueda': sqlAdd = ' WHERE p.cuerpo LIKE ?'; dato = '%' + dato + '%'; break;
             }
-            let sql = "SELECT* FROM preguntas AS p INNER JOIN formular AS f ON p.id = f.idPregunta INNER JOIN usuarios AS u ON u.id = f.idUsuario";
-            con.query(sql,
+            let sql = "SELECT* FROM preguntas AS p INNER JOIN formular AS f ON p.id = f.idPregunta INNER JOIN usuarios AS u ON u.id = f.idUsuario" + sqlAdd;
+            con.query(sql, [dato],
                 function (err, result) {
                     con.release();
                     if (err)
                         callback(error);
-                    else{
-                        let cont=0;
+                    else {
+                        let cont = 0;
                         result.forEach(pregunta => {
                             cont++;
-                            pregunta.foto = Buffer.from(pregunta.foto).toString('base64'); 
-                            pregunta.etiquetas =  pregunta.etiquetas.split( /@/);
-                            pregunta.fecha =  pregunta.fecha.getFullYear() + "-" + pregunta.fecha.getMonth() + "-" + pregunta.fecha.getDate();
+                            pregunta.foto = Buffer.from(pregunta.foto).toString('base64');
+                            pregunta.etiquetas = pregunta.etiquetas.split(/@/);
+                            pregunta.fecha = pregunta.fecha.getFullYear() + "-" + pregunta.fecha.getMonth() + "-" + pregunta.fecha.getDate();
                         });
                         result.cont = cont;
-                        
-                        response.render("preguntas.ejs", {usuario:request.session.usuario, preguntas:result});
+
+                        response.render("preguntas.ejs", { usuario: request.session.usuario, preguntas: result });
                     }
-                      
+
                 });
         }
     });
@@ -293,33 +297,33 @@ function obtenerPreguntas(tipoBusqueda, dato, request, response) {
 
 function insertarPregunta(pregunta, callback) {
     pool.getConnection(function (err, con) {
-        if (err){
+        if (err) {
             callback(err);
-        }else {
+        } else {
 
             let sql =
                 "INSERT INTO preguntas (titulo, cuerpo, etiquetas, fecha) VALUES(?, ?, ?, ?)";
             con.query(sql, [pregunta.titulo, pregunta.cuerpo,
             pregunta.etiquetas, pregunta.fecha],
-            function (err, result) {
-                if (err){
-                    callback(error);
-                }else {
-                let id = result;
-                    let sql =
-                        "INSERT into formular (idUsuario, idPregunta) VALUES (?, ?)";
-                    con.query(sql, [pregunta.usuarioId, result.insertId],
-                    function (err,response, result) {
-                        con.release();
-                        if (err){
-                            callback(error);
-                        }else {
-                            console.log("CREADA CORRECTAMENTE")
-                            callback(null);
-                        }                    
-                    });
-                }
-            });
+                function (err, result) {
+                    if (err) {
+                        callback(error);
+                    } else {
+                        let id = result;
+                        let sql =
+                            "INSERT into formular (idUsuario, idPregunta) VALUES (?, ?)";
+                        con.query(sql, [pregunta.usuarioId, result.insertId],
+                            function (err, response, result) {
+                                con.release();
+                                if (err) {
+                                    callback(error);
+                                } else {
+                                    console.log("CREADA CORRECTAMENTE")
+                                    callback(null);
+                                }
+                            });
+                    }
+                });
         }
     });
 }
